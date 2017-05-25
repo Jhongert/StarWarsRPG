ed4c67986946d51ff7863b66210497dafdf73ad2
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-05-25 00:19:05 -0400

    update

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -55,28 +55,12 @@ $(document).ready(function(){
 			$("#characters").append(div); //append the new div to id=characters
 
 			//inside the new div append a <p> with the character name,
-			
-
-
-
 			//character's image and Heath points
-
-
-			div.append($("<img>").attr("src", "assets/images/" + character[i]["img"]));
 			div.append($("<p>").text(character[i]["name"]));
-			div.append($("<p>").html("Health Points: <span>" + character[i]["hp"] + "</span>"))
-			div.append($("<p>").html("Attack Power: " + character[i]["attackPower"]));
-			div.append($("<p>").html("Counter Attack: " + character[i]["counterAttack"]))
+			div.append($("<img>").attr("src", "assets/images/" + character[i]["img"]));
+			div.append($("<span>").text(character[i]["hp"]));
 		}
 		attachOnClick(); //attach onclick event to characters
-	}
-
-	//add a progress bar to attacker and defender characters
-	function addProgressBar(className){
-		var progressbar = $("<div>").addClass("progress");
-		var div = $("<div>").addClass("progress-bar progress-bar-success").text("100%");
-		progressbar.append(div);
-		$(className).append(progressbar);
 	}
 
 	newGame(); //create a new game
@@ -93,7 +77,6 @@ $(document).ready(function(){
 	//after create characters elements
 	function attachOnClick() {
 		$(".character").on("click", function(){
-			
 			var currentCharacter = $(this); //character that has been clicked
 
 			//if there is not a attacker
@@ -101,29 +84,23 @@ $(document).ready(function(){
 				attackerId = currentCharacter.attr("id"); //get attacker id
 				attackerHp = character[attackerId].hp;    //get attacker heath points
 				currentCharacter.addClass("attacker");    //add class attacker
-				currentCharacter.off("click"); //remove click event from this character
 
 				//append the character to 'your character'(attacker) zone
 				$("#yourCharacter").append(currentCharacter); 
-
-				//add progress bar to attacker
-				addProgressBar(".attacker");
-
 				$("#topMsg").text("Enemies Available To Attack");
 
-				//if there is not a defender
-			}else if(defenderId == ""){
+				//add class enemy to the rest of the characters
+				$(".character:not(.attacker)").addClass("enemy");
+
+				//if there is not a defender and I am not clicking the attacker(if is a enemy)
+			}else if(defenderId == "" && currentCharacter.attr("id") != attackerId){
 				defenderId = currentCharacter.attr("id"); //get enemy id
 				defenderHp = character[defenderId].hp;    //get enemy heath points
 				currentCharacter.addClass("defender");    //add class defender
-				currentCharacter.off("click"); //remove click event from this character
 
 				//append character to defender zone
 				$("#defender").append(currentCharacter);  
 
-				//add progress bar to defender
-				addProgressBar(".defender");
-				
 				$("#attack").css("display", "inline"); //display button attack
 				msg.empty(); //clean mesages
 			}
@@ -133,50 +110,39 @@ $(document).ready(function(){
 	//button attack click
 	$("#attack").click(function(){
 		
-		//increase attacker's attackPower
-		attackPower += character[attackerId]["attackPower"];
-		defenderHp -= attackPower; //decrease defender hp
+			//increase attacker's attackPower
+			attackPower += character[attackerId]["attackPower"];
+			defenderHp -= attackPower; //decrease defender hp
 
-		//if defender hp is less than 1
-		if(defenderHp <= 0){
-			$("#" + defenderId).remove(); //remove the defender
-			$("#attack").css("display", "none"); //hide button attack
+			//if defender hp is less than 1
+			if(defenderHp <= 0){
+				$("#" + defenderId).remove(); //remove the defender
+				$("#attack").css("display", "none"); //hide button attack
 
-			enemyCount --; //decrease enemies count
+				enemyCount --; //decrease enemies count
 
-			//if there is not more enemies, you won
-			if(enemyCount == 0){
-				msg.html("<p>You Won!!!</p>");
-				$("#restart").css("display", "block");
-			}else{ //if still more enemies show a message and clean defenderId variable
-				msg.html("<p>You have defeated " + character[defenderId].name + ", you can choose to fight another enemy.</p>");
-				defenderId = "";
-			}
+				//if there is not more enemies, you won
+				if(enemyCount == 0){
+					msg.html("<p>You Won!!!</p>");
+					$("#restart").css("display", "block");
+				}else{ //if still more enemies show a message and clean defenderId variable
+					msg.html("<p>You have defeated " + character[defenderId].name + ", you can choose to fight another enemy.</p>");
+					defenderId = "";
+				}
 
-		}else{ //if defender still alive
-			$(".defender span").text(defenderHp); //update defender hp on screen
+			}else{ //if defender still alive
+				$(".defender span").text(defenderHp); //update defender hp on screen
+				attackerHp -= character[defenderId].counterAttack; //decrease attacker hp
+				$(".attacker span").text(attackerHp); //update attacker hp on screen
 
-			//update defender progres bar
-			var defHpPercent = defenderHp * 100 / character[defenderId].hp;
-			$(".defender .progress-bar").width(defHpPercent + "%").text(Math.round(defHpPercent) + "%");
-
-
-			attackerHp -= character[defenderId].counterAttack; //decrease attacker hp
-			$(".attacker span").text(attackerHp); //update attacker hp on screen
-		
-			//update attacker progress bar
-			var attHpPercent = attackerHp * 100 / character[attackerId].hp;
-			$(".attacker .progress-bar").width(attHpPercent + "%").text(Math.round(attHpPercent) + "%");;
-
-
-			if(attackerHp > 0){ //if attacker still have hp show message
-				msg.html("<p>You attacked " + character[defenderId]["name"] + " for "+ attackPower + " damage. <br>" +
-				character[defenderId]["name"] + " attacked you back for "+ character[defenderId]["counterAttack"] + " damage.</p>");
-			}else{ //if attacker's hp is less than 1 then game over
-				msg.html("<p>You have been defeated...</P>");
-				$("#attack").css("display", "none");
-				$("#restart").css("display", "block");
-			}
-		}		
+				if(attackerHp > 0){ //if attacker still have hp show message
+					msg.html("<p>You attacked " + character[defenderId]["name"] + " for "+ attackPower + " damage. <br>" +
+					character[defenderId]["name"] + " attacked you back for "+ character[defenderId]["counterAttack"] + " damage.</p>");
+				}else{ //if attacker's hp is less than 1 then game over
+					msg.html("<p>You have been defeated...</P>");
+					$("#attack").css("display", "none");
+					$("#restart").css("display", "block");
+				}
+			}		
 	});
 });
